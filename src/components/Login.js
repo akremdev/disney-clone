7f2001fc14a5f2abfc66a6554322c83833d1934c
akremdev <akremdev@gmail.com>
2021-04-25 14:37:50 +0100

    Add Logos Images + styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,11 @@ const Login = (props) => {
     return (
         <Container>
         <Content>
-            Content
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg" alt=""
+                />
+            </CTA>
+            <BgImage />
         </Content>
     </Container>
     )
@@ -24,7 +28,51 @@ const Content = styled.div`
     width: 100%;
     position: relative; 
     min-height: 100vh;
-    
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direciton : column;
+    padding: 80px 40px;
+    height: 100%;
 `;
+
+const BgImage = styled.div`
+    background-image: url("/images/login-background.jpg");
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    top:0;
+    left:0;
+    right:0;
+    z-index: -1;
+`;
+
+const CTA = styled.div`
+    margin-bottom: 2vw;
+    max-width: 650px;
+    flex-wrap: wrap;
+    display: flex;
+    flex-direction : column;
+    justify-content: center;
+    margin-top: 0;
+    align-items: center;
+    text-align: center;
+    margin-right: auto;
+    margin-left: auto;
+    transition-timing-function: ease-out;
+    transition: opactiy 0.2s;
+    width: 100%;
+`
+
+const CTALogoOne = styled.img`
+    margin-bottom: 12px;
+    max-width: 600px;
+    min-height: 1px;
+    display:block;
+    width: 100%;
+`
 
 export default Login;
