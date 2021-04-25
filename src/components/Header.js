1563b264f230b558049ba6653cdf69044414457d
akremdev <akremdev@gmail.com>
2021-04-25 18:22:20 +0100

    setup firebase auth + db

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
+import { auth, provider } from '../firebase';
 
 
 const Header = (props) => {
+
+    const handleAuth = () => {
+        auth
+        .signInWithPopup(provider)
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((error) => {
+            alert(error.message);
+        })
+    }
+
     return (
         <Nav>
             <Logo>
@@ -33,8 +46,8 @@ const Header = (props) => {
               <img src="/images/series-icon.svg" alt="SERIES" />
               <span>SERIES</span>
             </a>
-            
             </NavMenu>
+            <Login onClick={handleAuth}>Login</Login>
         </Nav>
     )
 }
@@ -124,5 +137,20 @@ const NavMenu = styled.div`
   /* @media (max-width: 768px) {
     display: none;
   } */
+`;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
 `;
 export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJqORRXAcY_uLTdSddodUUC7j-ClwFAHc",
+    authDomain: "disneyplus-1ef01.firebaseapp.com",
+    projectId: "disneyplus-1ef01",
+    storageBucket: "disneyplus-1ef01.appspot.com",
+    messagingSenderId: "260760569541",
+    appId: "1:260760569541:web:d15dc6e417876e5e7612ac",
+    measurementId: "G-FZVC1PRZPD"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth =  firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const storage = firebase.storage();
+
+  export {auth, provider, storage}
+  export default db;
